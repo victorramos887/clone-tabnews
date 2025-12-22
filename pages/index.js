b5f9389f6035bc60011f0bc6c9a11d3f8cf150f4
Victor Ramos <v.ramos58@hotmail.com>
2025-12-22 07:54:57 -0300

    feat: alterando farase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Sonhar grande e sonhar pequeno tem o mesmo esforço</h1>
+    return <h1>
+        Como dizia Carl Sagan, “em algum lugar, algo incrível está esperando para ser descoberto”.
+        Eu sei que esse algo é a nossa vitória.
+
+        Esse é o ano das nossas conquistas. Vamos vencer juntos. 🤍
+    </h1>
 }
 
 export default Home;
