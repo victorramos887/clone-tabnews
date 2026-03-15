1dc3f6f2215ed1276b7090d284a451756d79d82f
Victor Ramos <v.ramos58@hotmail.com>
2026-03-15 15:03:31 -0300

    makes database.js client.conneti() more robust to erros

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,22 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
   });
-  await client.connect();
+
+  console.log("Credencias do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  })
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     await client.end();
     return result;
   } catch (err) {
     console.log(err);
+    throw err;
   } finally {
     await client.end();
   }
