f5a039f4624deb5c7e8af3b5c98773708d28e19d
Victor Ramos <v.ramos58@hotmail.com>
2026-03-15 11:42:14 -0300

    make database.js many robuster

--- infra/database.js
+++ infra/database.js
@@ -1,21 +1,25 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
 async function query(queryObject) {
-    
-    const client = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-    });
-
-    await client.connect();
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  });
+  await client.connect();
+  try {
     const result = await client.query(queryObject);
     await client.end();
     return result;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
-    query: query,
+  query: query,
 };
